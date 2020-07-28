c8829570f32e0fb62eaace411e7daf534af9ea67
Pierre Remy <68400053+Pierre-Remy@users.noreply.github.com>
2020-07-28 10:48:52 +0200

    Updating contact page javascript for reviewing.
    This is the right version to review.

--- contact.js
+++ contact.js
@@ -1,27 +1,25 @@
-const name1= document.getElementById("name1");
-const email1= document.getElementById("email1");
-const message= document.getElementById("message");
-const form= document.getElementById ("form")
-const error= document.getElementById ("error")
-   
-form.addEventListener ('submit', (e) => {
+function validate () {
 
-        let messages= [];
-        //Checking for errors in name space//
+ var name1 = document.getElementById("name1");
+ var email1 = document.getElementById("email1");
+ var message = document.getElementById("message");
 
-        if (name1 === "" || x == null) {
-            messages.push("Please, the name cannot be blank");
-        }
-          
-          if(messages.length < 0 ) {
-            errorElement.innertext= messages.join(",")
-            e.preventDefault();
-          } 
-          if (name1 === "" || x == null) {
-            messages.push("Please, the email cannot be blank");
-        }
-        
-        if (message === "" || message==="Type your message here!") {
-            messages.push("Please, Type something in message space!");
-        }
-        })
+ //checking the name
+if(name1.value.trim() === "") {
+  alert ("Please, fill in your name");
+      return false;
+  
+} 
+//checking email
+if (email1.value.trim() === "") {
+  alert ("Please, fill in your email");
+  return false;
+}
+  //checking message
+
+if(message.value.trim() === "")
+{
+  alert ("Please, leave message");
+      return false;
+}
+}
