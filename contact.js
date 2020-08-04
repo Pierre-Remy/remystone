c48063794218a771eb66bb11e9d387e0aec39a46
Pierre Remy <pierreremynshimiyimana143@gmail.com>
2020-08-04 13:36:28 +0200

    contact page changed

--- contact.js
+++ contact.js
@@ -1,25 +1,28 @@
-function validate () {
+document.getElementById('form').addEventListener('submit', validate);
 
- var name1 = document.getElementById("name1");
- var email1 = document.getElementById("email1");
- var message = document.getElementById("message");
+function Validate(e) {
+  e.preventDefault();
 
- //checking the name
+var name1 = document.getElementById("name1");
+var email1 = document.getElementById("email1");
+var message = document.getElementById("message");
+
+//checking the name
 if(name1.value.trim() === "") {
-  alert ("Please, fill in your name");
-      return false;
-  
+ alert ("Please, fill in your name");
+     return false;
+ 
 } 
 //checking email
 if (email1.value.trim() === "") {
-  alert ("Please, fill in your email");
-  return false;
+ alert ("Please, fill in your email");
+ return false;
 }
-  //checking message
+ //checking message
 
 if(message.value.trim() === "")
 {
-  alert ("Please, leave message");
-      return false;
+ alert ("Please, leave message");
+     return false;
 }
 }
