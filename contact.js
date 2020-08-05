2e1a6820fcb62f4774e65d56cd966018acfa4b34
Pierre Remy <pierreremynshimiyimana143@gmail.com>
2020-08-05 16:06:53 +0200

    Contact page being connected to firebase

--- contact.js
+++ contact.js
@@ -1,28 +1,76 @@
-document.getElementById('form').addEventListener('submit', validate);
+// Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyBuGL2QzDvZqoRgKwJLpwmS2s5CdnQ6sd0",
+      authDomain: "contactform-f979d.firebaseapp.com",
+      databaseURL: "https://contactform-f979d.firebaseio.com",
+      projectId: "contactform-f979d",
+      storageBucket: "contactform-f979d.appspot.com",
+      messagingSenderId: "548522574947",
+      appId: "1:548522574947:web:f5db46da2c6c184d252456"
+    };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  var messagesRef = firebase.firestore();
 
-function Validate(e) {
-  e.preventDefault();
+// Listen for form submit
+  document.getElementById('button').addEventListener('click', submitForm);
 
-var name1 = document.getElementById("name1");
-var email1 = document.getElementById("email1");
-var message = document.getElementById("message");
+// Submit form
+  function submitForm(e){
+    e.preventDefault();
 
-//checking the name
-if(name1.value.trim() === "") {
- alert ("Please, fill in your name");
-     return false;
- 
-} 
-//checking email
-if (email1.value.trim() === "") {
- alert ("Please, fill in your email");
- return false;
-}
- //checking message
+    //Get value
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var message = getInputVal('message');
 
-if(message.value.trim() === "")
-{
- alert ("Please, leave message");
-     return false;
-}
-}
+  //validate email
+  var dotposition=email.lastIndexOf("."); 
+  var atposition=email.indexOf("@");  
+
+  if(name.trim() === "" || email.trim() ==="" || message.trim()==="" ) {
+      alert("all field must be filled");
+      return false;
+  } else if (atposition<1 || dotposition<atposition+2 || dotposition+2>=email.length){  
+    alert("Please enter a valid e-mail address");
+    return false;  
+    } 
+    else{
+
+      return true;
+    };
+
+
+
+    // Save message
+    saveMessage(name,  email, message);
+
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // Hide alert after 5 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },5000);
+
+    // Clear form
+    document.getElementById('form').reset();
+      }
+      // Function to get form value
+      function getInputVal(id){
+        return document.getElementById(id).value;
+      }
+      // Save message to firebase
+      function saveMessage(name, email, message){
+        var newMessageRef = messagesRef.collection("Contactform").add({
+          name: name,
+          email: email,
+          message: message
+        }).then(function(docRef) {
+          console.log("information submited(name,email,message)"+ name +"," +email+ ","+ message);
+      })
+      .catch(function(error) {
+          console.error("There is error in the document.", error);
+      });
+      }
